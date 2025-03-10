34b60cf00cee9a652491931dae37eda2d88ae479
Nice3point <nice3point@gmail.com>
2025-03-10 15:18:53 +0300

    Sync Ui abstractions

--- source/RevitLookup.UI.Abstractions/NavigationException.cs
+++ source/RevitLookup.UI.Abstractions/NavigationException.cs
@@ -11,7 +11,7 @@ namespace Wpf.Ui.Abstractions;
 public sealed class NavigationException : Exception
 {
     /// <summary>
-    /// Initializes a new instance of the NavigationException class with a specified error message.
+    /// Initializes a new instance of the <see cref="NavigationException"/> class with a specified error message.
     /// </summary>
     /// <param name="message">The message that describes the error.</param>
     public NavigationException(string message)
@@ -20,7 +20,8 @@ public sealed class NavigationException : Exception
     }
 
     /// <summary>
-    /// Initializes a new instance of the NavigationException class with a specified error message and a reference to the inner exception that is the cause of this exception.
+    /// Initializes a new instance of the <see cref="NavigationException"/> class with a specified error message
+    /// and a reference to the inner exception that is the cause of this exception.
     /// </summary>
     /// <param name="e">The exception that is the cause of the current exception.</param>
     /// <param name="message">The message that describes the error.</param>
