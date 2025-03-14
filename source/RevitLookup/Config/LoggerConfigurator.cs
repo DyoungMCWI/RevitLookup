0bc863e697febe96afe8d8dea29067af2b4eb824
Nice3point <nice3point@gmail.com>
2025-03-15 00:18:31 +0300

    Add summary

--- source/RevitLookup/Config/LoggerConfigurator.cs
+++ source/RevitLookup/Config/LoggerConfigurator.cs
@@ -6,6 +6,20 @@ using Serilog.Events;
 
 namespace RevitLookup.Config;
 
+/// <summary>
+///     Application logging configuration
+/// </summary>
+/// <example>
+/// <code lang="csharp">
+/// public class Class(ILogger&lt;Class&gt; logger)
+/// {
+///     private void Execute()
+///     {
+///         logger.LogInformation("Message");
+///     }
+/// }
+/// </code>
+/// </example>
 public static class LoggerConfigurator
 {
     private const string LogTemplate = "{Timestamp:yyyy-MM-dd HH:mm:ss} [{Level:u3}] {SourceContext}: {Message:lj}{NewLine}{Exception}";
@@ -16,7 +30,7 @@ public static class LoggerConfigurator
         builder.AddSerilog(logger);
 
         PresentationTraceSources.ResourceDictionarySource.Switch.Level = SourceLevels.Critical;
-        AppDomain.CurrentDomain.UnhandledException += OnOnUnhandledException;
+        AppDomain.CurrentDomain.UnhandledException += OnUnhandledException;
     }
 
     private static Logger CreateDefaultLogger()
@@ -29,7 +43,7 @@ public static class LoggerConfigurator
             .CreateLogger();
     }
 
-    private static void OnOnUnhandledException(object sender, UnhandledExceptionEventArgs args)
+    private static void OnUnhandledException(object sender, UnhandledExceptionEventArgs args)
     {
         var exception = (Exception) args.ExceptionObject;
         var logger = Host.GetService<ILogger<AppDomain>>();
